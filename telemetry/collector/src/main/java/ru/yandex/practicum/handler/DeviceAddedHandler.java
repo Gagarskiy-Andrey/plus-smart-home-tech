cc19a180d2512796c6f19c45810e1b24fe0733f1
GrimJak <a.gagarskiy@yandex.kz>
2025-07-19 16:04:11 +0300

    ТЗ 20 (промежуточное)

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/handler/DeviceAddedHandler.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.handler;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.grpc.telemetry.event.HubEventProto.PayloadCase;
+import ru.yandex.practicum.kafka.KafkaAvroProducer;
+import ru.yandex.practicum.mappers.EventMapper;
+
+@Component
+@RequiredArgsConstructor
+@Getter
+@Slf4j
+public class DeviceAddedHandler implements HubEventHandler {
+
+    private final EventMapper mapper;
+    private final KafkaAvroProducer producer;
+
+    @Override
+    public PayloadCase getHandledType() {
+        return PayloadCase.DEVICE_ADDED;
+    }
+}
