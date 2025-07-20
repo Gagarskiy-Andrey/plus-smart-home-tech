52e50e71bf84c86f4025f8aaa0a14f5d26d0bbf9
GrimJak <a.gagarskiy@yandex.kz>
2025-07-20 10:43:23 +0300

    ТЗ 20 (еще одно промежуточное)

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/KafkaAvroProducer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/KafkaAvroProducer.java
@@ -12,13 +12,12 @@ import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.serializer.BaseAvroSerializer;
 
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
 public class KafkaAvroProducer {
 
-    private final KafkaProducer<String, Object> kafkaProducer;
+    private final KafkaProducer<String, byte[]> kafkaProducer;
 
     private final BaseAvroSerializer<GenericContainer> avroSerializer = new BaseAvroSerializer<>();
 
@@ -29,56 +28,44 @@ public class KafkaAvroProducer {
     private String hubTopic;
 
     @Autowired
-    public KafkaAvroProducer(KafkaProducer<String, Object> kafkaProducer) {
+    public KafkaAvroProducer(KafkaProducer<String, byte[]> kafkaProducer) {
         this.kafkaProducer = kafkaProducer;
     }
 
     public void sendSensorEvent(SensorEventAvro sensorEvent) {
-
         byte[] eventBytes = avroSerializer.serialize(sensorTopic, sensorEvent);
-
         long timeStamp = sensorEvent.getTimestamp().toEpochMilli();
-
         String key = sensorEvent.getHubId();
 
-        log.info("Отправка Avro-сообщения в топик {}: Ключ='{}', Значение='{}'", sensorTopic, key, sensorEvent);
+        log.info("📤 Отправка CLIMATE_SENSOR_EVENT: ключ='{}', размер={} байт", key, eventBytes.length);
 
-        ProducerRecord<String, Object> record = new ProducerRecord<>(sensorTopic, null, timeStamp, key, eventBytes);
-        CompletableFuture.runAsync(() -> {
-            try {
-                RecordMetadata metadata = kafkaProducer.send(record).get();
-                log.info("Сообщение успешно отправлено в топик '{}', раздел {}, смещение {}",
-                        metadata.topic(),
-                        metadata.partition(),
-                        metadata.offset());
-            } catch (Exception ex) {
-                log.error("Не удалось отправить сообщение: {}", ex.getMessage(), ex);
-            }
-        });
+        ProducerRecord<String, byte[]> record = new ProducerRecord<>(sensorTopic, null, timeStamp, key, eventBytes);
+
+        try {
+            RecordMetadata metadata = kafkaProducer.send(record).get(); // синхронно
+            log.info("Успешно отправлено в '{}': partition={}, offset={}",
+                    metadata.topic(), metadata.partition(), metadata.offset());
+        } catch (Exception ex) {
+            log.error("Ошибка при отправке SensorEvent: {}", ex.getMessage(), ex);
+        }
     }
 
     public void sendHubEvent(HubEventAvro hubEvent) {
-
         byte[] eventBytes = avroSerializer.serialize(hubTopic, hubEvent);
-
         long timeStamp = hubEvent.getTimestamp().toEpochMilli();
-
         String key = hubEvent.getHubId();
 
-        log.info("Отправка Avro-сообщения в топик {}: Ключ='{}', Значение='{}'", hubTopic, key, hubEvent);
+        log.info("📤 Отправка HubEvent: ключ='{}', размер={} байт", key, eventBytes.length);
 
-        ProducerRecord<String, Object> record = new ProducerRecord<>(hubTopic, null, timeStamp, key, eventBytes);
-        CompletableFuture.runAsync(() -> {
-            try {
-                RecordMetadata metadata = kafkaProducer.send(record).get();
-                log.info("Сообщение успешно отправлено в топик '{}', раздел {}, смещение {}",
-                        metadata.topic(),
-                        metadata.partition(),
-                        metadata.offset());
-            } catch (Exception ex) {
-                log.error("Не удалось отправить сообщение: {}", ex.getMessage(), ex);
-            }
-        });
+        ProducerRecord<String, byte[]> record = new ProducerRecord<>(hubTopic, null, timeStamp, key, eventBytes);
+
+        try {
+            RecordMetadata metadata = kafkaProducer.send(record).get();
+            log.info("Успешно отправлено в '{}': partition={}, offset={}",
+                    metadata.topic(), metadata.partition(), metadata.offset());
+        } catch (Exception ex) {
+            log.error("Ошибка при отправке HubEvent: {}", ex.getMessage(), ex);
+        }
     }
 }
 
