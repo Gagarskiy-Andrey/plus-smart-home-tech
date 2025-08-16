6c45989a59e1f283afdfc3ff82c14aa19dcdcfde
GrimJak <a.gagarskiy@yandex.kz>
2025-08-16 16:28:04 +0300

    Слияние develop с main

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/smarthometech/aggregator/kafka/SensorEventKafkaListener.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/smarthometech/aggregator/kafka/SensorEventKafkaListener.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.smarthometech.aggregator.kafka;
 
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
